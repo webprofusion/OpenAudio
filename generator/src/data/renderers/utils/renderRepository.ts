c91b6c32c7eeb6f82ae938aae0353d72de786250
Christopher Cook <christopher.cook@webprofusion.com>
2026-01-12 10:36:23 +0800

    Experiment with issue template based submissions

--- generator/src/data/renderers/utils/renderRepository.ts
+++ generator/src/data/renderers/utils/renderRepository.ts
@@ -3,22 +3,40 @@ import {type Repository} from '../../types';
 
 export const renderRepository = (repository: Repository) => {
   switch (repository.type) {
+    case 'Assembla':
+      return renderLink(
+        `Assembla → ${repository.space}`,
+        `https://app.assembla.com/spaces/${repository.space}/git/source`,
+      );
+    case 'Bitbucket':
+      return renderLink(
+        `${repository.workspace}/${repository.repo}`,
+        `https://bitbucket.org/${repository.workspace}/${repository.repo}`,
+      );
+    case 'Codeberg':
+      return renderLink(
+        `${repository.user}/${repository.repo}`,
+        `https://codeberg.org/${repository.user}/${repository.repo}`,
+      );
     case 'GitHub':
       return renderLink(
         `${repository.user}/${repository.repo}`,
         `https://github.com/${repository.user}/${repository.repo}`,
+      );
+    case 'GitLab':
+      return renderLink(
+        `${repository.user}/${repository.repo}`,
+        `https://gitlab.com/${repository.user}/${repository.repo}`,
       );
     case 'SourceForge':
       return renderLink(
         `SourceForge → ${repository.project}`,
         `https://sourceforge.net/projects/${repository.project}`,
       );
-    case 'Assembla':
+    case 'SourceHut':
       return renderLink(
-        `Assembla → ${repository.space}`,
-        `https://app.assembla.com/spaces/${repository.space}/git/source`,
+        `~${repository.user}/${repository.repo}`,
+        `https://sr.ht/~${repository.user}/${repository.repo}`,
       );
-    default:
-      return '';
   }
 };
