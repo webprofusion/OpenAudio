73d31e53a90627b60549ac29fd3e42a780595e75
Christopher Cook <development@webprofusion.com>
2026-01-12 12:39:17 +0800

    Update contribution guidelines in index.ts
    Removed instructions for directly editing README and collection files.

--- generator/src/index.ts
+++ generator/src/index.ts
@@ -54,21 +54,7 @@ https://openaudio.webprofusion.com
 <details>
 <summary>Contributing links</summary>
 
-Our site and README are generated from the lists defined in code. **Links are no longer edited directly in the README.** To contribute links you will need to modify the collection you want to update and submit that to us with a new GitHub Pull Request (PR). You can do that directly on GitHub by clicking the *Edit* button on the top right of the page when viewing any of the collection files:
-
-_Do not edit any other files as updating a generated file will cause your changes to be lost._
-
-Choose the collection to edit:
-
-<ul>
-  <li><a href="data/plugins.json">Audio Plugins</a></li>
-  <li><a href="data/collections.json">Collections</a></li>
-  <li><a href="data/apps.json">Audio Apps</a></li>
-  <li><a href="data/libraries.json">Software Development Libraries</a></li>
-  <li><a href="data/samples.json">Code Samples</a></li>
-  <li><a href="data/resources.json">Open Data Resources</a></li>
-</ul>
-</details>
+To contribute new links, fill out our issue template and the change will be prepare automatically and reviewed by an editor: https://github.com/webprofusion/OpenAudio/issues 
 
 Audio Plugins
 -------------
